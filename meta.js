4900a5b92a0e3d396a6cf739cde91e0d17fdf906
Jhon Andrew Q. Baes <anecbook@gmail.com>
2017-07-23 09:12:28 +0800

    Fixed line break on completeMessage.

--- meta.js
+++ meta.js
@@ -1,3 +1,3 @@
 module.exports = {
-  'completeMessage': 'Hi! This is Jhon Andrew. Thank you for using the vue-materialize.\n\n{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}\n\nAfter running `npm run dev` you will see links to guides on how to use MaterializeCSS and other libraries used in this boilerplate.'
+  'completeMessage': 'Hi! This is Jhon Andrew. Thank you for using the vue-materialize.\n\n{{#inPlace}}To get started:\n\n  npm install\n  npm run dev.{{else}}To get started:\n\n  cd {{destDirName}}\n  npm install\n  npm run dev.{{/inPlace}}\n\nAfter running `npm run dev` you will see links to guides on how to use\nMaterializeCSS and other libraries used in this boilerplate.'
 }
